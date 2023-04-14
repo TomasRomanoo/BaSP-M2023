5f2e038fc6e0bf7971db5986afbb7e1c5b6b7c0f
TomasRomanoo <romano.tomas@outlook.com.ar>
2023-04-14 11:06:27 -0300

    BaSP-W05-2023: exercise 01 done

---
+++ Week-05/exercises/01-variables-and-operator.js
@@ -0,0 +1,25 @@
+//      A - Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 
+//  3er variable.
+
+    var num1 = 20;
+    var num2 = 30;
+    var suma = num1 + num2;
+
+    console.log("El resultado del ej 01-A = " + suma);
+    
+//      B - Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+    var word1 = 'Pepa';
+    var word2 = 'Pig';
+    var sentence = word1 + word2;
+
+    console.log("El resultado del ej 01-B = " + sentence);
+
+//      C - Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando 
+//  el resultado de la suma en una 3er variable (utilizar length).
+
+    var name1 = 'Tomas';
+    var name2 = 'Pedro';
+    var totalLength = name1.length + name2.length;
+
+    console.log("El resultado del ej 01-C = " + totalLength);
