24e6cf6ef49306ea335cfdb69ee8f493da5e8ae1
TomasRomanoo <romano.tomas@outlook.com.ar>
2023-05-01 10:35:02 -0300

    BaSP-W07-2023: login done

--- Week-07/js/login.js
+++ Week-07/js/login.js
@@ -3,6 +3,7 @@ var emailExpression = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
 var emailInput = document.querySelector('input[name="email"]');
 var passwordInput = document.querySelector('input[name="password"]');
 var btnLogin = document.querySelector('#btnlogin');
+replacePlaceHolder()
 
 emailInput.addEventListener('blur',function(event){
     var element = document.querySelector('.emailError');
@@ -35,15 +36,14 @@ passwordInput.addEventListener('focus',function(){
 btnLogin.addEventListener('click', function(e){
     e.preventDefault();
     if(hasNumbersAndChar(passwordInput.value) && emailExpression.test(emailInput.value)){
-        alert("WELCOME!!\n Your email is: " + emailInput.value + "\n Your password is: " + passwordInput.value);
-    }else if((!hasNumbersAndChar(passwordInput.value) && !emailExpression.test(emailInput.value))){
-        alert("ERROR!!\n Wrong Email and Wrong Password  ")
-    }else if(hasNumbersAndChar(passwordInput.value)){
-        alert("ERROR!!\n Your email must respect this format: abc@abc.com")
-    }else if(!emailExpression.test(emailInput)){
-        alert("ERROR!!\n Your password must have letters and numbers ")
+        fetchGet( "https://api-rest-server.vercel.app/login?email="+emailInput.value+"&password="+passwordInput.value);
+        replacePlaceHolder();
     }
 })
+function replacePlaceHolder(){
+    if(localStorage.getItem("Email") != null){document.getElementById("email").value = localStorage.getItem("Email")}
+    if(localStorage.getItem("Password") != null){document.getElementById("pw").value = localStorage.getItem("Password")}
+};
 
 function hasNumbers(myString) {
     var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
@@ -72,3 +72,24 @@ function hasNumbersAndChar(myString) {
     }
     return false;
 }
+
+function fetchGet(url) {
+    return fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (resp) {
+            if(!resp.success){
+            throw new Error(JSON.stringify(resp))
+            }
+            alert("The request was successful:\n" + JSON.stringify(resp));
+            alert("Email: " + emailInput.value + "\nPassword: " + passwordInput.value);
+            localStorage.setItem("Email",emailInput.value);
+            localStorage.setItem("Password", passwordInput.value);
+        })
+        .catch(function (err) {
+            alert("The request could not be performed successfully:\n" + err);
+        });
+}
+
+
