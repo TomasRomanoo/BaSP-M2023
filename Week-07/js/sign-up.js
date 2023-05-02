8a5ae289c882b9c925203ac06914ceebf1448acb
TomasRomanoo <romano.tomas@outlook.com.ar>
2023-05-02 19:02:26 -0300

    BaSP-W07-2023: signUp done

--- Week-07/js/sign-up.js
+++ Week-07/js/sign-up.js
@@ -16,32 +16,31 @@ replacePlaceHolder();
 
 btnRegister.addEventListener('click', function(e){
     e.preventDefault(e);
-    var errors = [];
-    var good = [];
-    var empty = [];
+    var errorsArr = [];
+    var goodArr = [];
+    var emptyArr = [];
 
-    if(name1 == 'bad'){errors.push("name")}else if(name1 == '' || name1 == ' '){empty.push("Name")}else{good.push(name1)}
-    if(lastname == 'bad'){errors.push("Last Name")}else if(lastname == '' || lastname == ' '){empty.push("Last Name")}else{good.push(lastname)}
-    if(dni == 'bad'){errors.push("Dni")}else if(dni == '' || dni == ' ' ){empty.push("Dni")}else{good.push(dni)}
-    if(date == 'bad'){errors.push("Date")}else if(date == '' || date == ' '){empty.push("Date")}else{good.push(date)}
-    if(number1 == 'bad'){errors.push("Phone Number")}else if(number1 == '' || number1 == ' '){empty.push("Phone Number")}else{good.push(number1)}
-    if(address == 'bad'){errors.push("address")}else if(address == '' || address == ' '){empty.push("address")}else{good.push(address)}
-    if(location1 == 'bad'){errors.push("Location")}else if(location1 == '' || location1 == ' '){empty.push("Location")}else{good.push(location1)}
-    if(zipCode == 'bad'){errors.push("Zip Code")}else if(zipCode == '' || zipCode == ' '){empty.push("Zip Code")}else{good.push(zipCode)}
-    if(password == 'bad'){errors.push("Password")}else if(password == '' || password == ' '){empty.push("Password")}else{good.push(password)}
-    if(rePassword == 'bad'){errors.push("Password Validate")}else if(rePassword == '' || rePassword == ' '){empty.push("Password Validate")}else{good.push(rePassword)}
+    if(name1 == 'wrongInput'){errorsArr.push("name")}else if(name1 == '' || name1 == ' '){emptyArr.push("Name")}else{goodArr.push(name1)}
+    if(lastname == 'wrongInput'){errorsArr.push("Last Name")}else if(lastname == '' || lastname == ' '){emptyArr.push("Last Name")}else{goodArr.push(lastname)}
+    if(dni == 'wrongInput'){errorsArr.push("Dni")}else if(dni == '' || dni == ' ' ){emptyArr.push("Dni")}else{goodArr.push(dni)}
+    if(date == 'wrongInput'){errorsArr.push("Date")}else if(date == '' || date == ' '){emptyArr.push("Date")}else{goodArr.push(date)}
+    if(number1 == 'wrongInput'){errorsArr.push("Phone Number")}else if(number1 == '' || number1 == ' '){emptyArr.push("Phone Number")}else{goodArr.push(number1)}
+    if(address == 'wrongInput'){errorsArr.push("address")}else if(address == '' || address == ' '){emptyArr.push("address")}else{goodArr.push(address)}
+    if(location1 == 'wrongInput'){errorsArr.push("Location")}else if(location1 == '' || location1 == ' '){emptyArr.push("Location")}else{goodArr.push(location1)}
+    if(zipCode == 'wrongInput'){errorsArr.push("Zip Code")}else if(zipCode == '' || zipCode == ' '){emptyArr.push("Zip Code")}else{goodArr.push(zipCode)}
+    if(password == 'wrongInput'){errorsArr.push("Password")}else if(password == '' || password == ' '){emptyArr.push("Password")}else{goodArr.push(password)}
+    if(rePassword == 'wrongInput'){errorsArr.push("Password Validate")}else if(rePassword == '' || rePassword == ' '){emptyArr.push("Password Validate")}else{goodArr.push(rePassword)}
 
-    if(errors.length > 0){
-        alert("You have Errors in " + errors);
-    }else if(empty.length > 0 ){
-        alert("You have Empty inputs in: " + empty);
+    if(errorsArr.length > 0){
+        alert("You have Errors in " + errorsArr);
+    }else if(emptyArr.length > 0 ){
+        alert("You have empty inputs in: " + emptyArr);
     }else{
         fetchGet("https://api-rest-server.vercel.app/signup?name="+nameInput.value+"&lastName="+lastNameInput.value+
         "&dni="+dniInput.value+"&dob="+date+"&phone="+phoneInput.value+"&address="+addressInput.value+
         "&city="+locationInput.value+"&zip="+zipCodeInput.value+"&email="+emailInput.value+"&password="+passwordInput.value);
         replacePlaceHolder();
     }
-        
 });
 
 
@@ -100,13 +99,13 @@ function replacePlaceHolder(){
 nameInput.addEventListener('blur', function(event){
     var name = event.target.value;
     var element = document.querySelector('.nameError');
-
-    for(let i = 0; i < name.length; i++) {
+    if(name == ''){name = ' '}
+    for(var i = 0; i < name.length; i++) {
         var character = name[i];
-        if(typeof character !== 'string' || !isNaN(parseInt(character)) || (name.length <= 3) ) {
+        if(typeof character !== 'string' || !isNaN(parseInt(character)) || name.length <= 3){
             element.classList.remove('messageError');
             element.classList.add('showError');
-            name1 = 'bad';
+            name1 = 'wrongInput';
         }else{name1 = event.target.value};
     };
 });
@@ -121,13 +120,13 @@ nameInput.addEventListener('focus',function(){
 lastNameInput.addEventListener('blur', function(event){
     var name = event.target.value;
     var element = document.querySelector('.lastnameError');
-
+    if(name == ''){name = ' '}
     for(var i = 0; i < name.length; i++) {
         var character = name[i];
         if(typeof character !== 'string' || !isNaN(parseInt(character)) || name.length <= 3){
             element.classList.remove('messageError');
             element.classList.add('showError');
-            lastname= 'bad';
+            lastname= 'wrongInput';
         }else{lastname = event.target.value};
     }
 });
@@ -142,12 +141,12 @@ dniInput.addEventListener('blur', function(event){
     var number = event.target.value;
     var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
     var element = document.querySelector('.dniError');
-
+    if(number == ''){number = ' '}
     for(var i = 0; i < number.length; i++){
         if(!numbers.includes(number[i]) || number.length <=7){
             element.classList.remove('messageError');
             element.classList.add('showError');
-            dni = 'bad';
+            dni = 'wrongInput';
         }else{dni = event.target.value;}
     }
 });
@@ -162,12 +161,12 @@ phoneInput.addEventListener('blur', function(event){
     var number = event.target.value;
     var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
     var element = document.querySelector('.numberError');
-
+    if(number == ''){number = ' '}
     for(var i = 0; i < number.length; i++){
         if(!numbers.includes(number[i]) || (number.length !== 10)){
             element.classList.remove('messageError');
             element.classList.add('showError');
-            number1 = 'bad';
+            number1 = 'wrongInput';
         }else{number1 = event.target.value;}
     };
 });
@@ -183,7 +182,7 @@ dateInput.addEventListener('blur', function(event){
     if(event.target.value == ''){
         element.classList.remove('messageError');
         element.classList.add('showError');
-        date = 'bad';
+        date = 'wrongInput';
     }else{
         date = event.target.value
         element.classList.remove('showError');
@@ -205,7 +204,7 @@ locationInput.addEventListener('blur', function(event){
         event.target.value.length <= 3){
         element.classList.remove('messageError');
         element.classList.add('showError');
-        location1 = 'bad';
+        location1 = 'wrongInput';
     }else{location1 = event.target.value;}
 });
 
@@ -221,7 +220,7 @@ addressInput.addEventListener('blur', function(event){
         hasSpace(event.target.value)){
         element.classList.remove('messageError');
         element.classList.add('showError');
-        address = 'bad';
+        address = 'wrongInput';
     }else{address = event.target.value;}
 });
 
@@ -235,12 +234,12 @@ zipCodeInput.addEventListener('blur', function(event){
     var element = document.querySelector('.zipError');
     var number = event.target.value;
     var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-
+    if(number == ''){number = ' '}
     for(var i = 0; i < number.length; i++){
         if(!numbers.includes(number[i]) || number.length !== 5 && number.length !== 4 ){
             element.classList.remove('messageError');
             element.classList.add('showError');
-            zipCode = 'bad';
+            zipCode = 'wrongInput';
         }else{zipCode = event.target.value;}
     }
 });
@@ -256,7 +255,7 @@ emailInput.addEventListener('blur', function(event){
     if(!emailExpression.test(event.target.value)){
         element.classList.remove('messageError');
         element.classList.add('showError');
-        email = 'bad';
+        email = 'wrongInput';
     }else{email = event.target.value;}
 });
 
@@ -271,7 +270,7 @@ passwordInput.addEventListener('blur', function(event){
     if(!hasNumbersAndChar(event.target.value) || event.target.value.length < 8){
         element.classList.remove('messageError');
         element.classList.add('showError');
-        password = 'bad';
+        password = 'wrongInput';
     }else{password = event.target.value;}
     
 });
@@ -288,7 +287,7 @@ rePasswordInput.addEventListener('blur', function(event){
         event.target.value !== password){
         element.classList.remove('messageError');
         element.classList.add('showError');
-        rePassword = 'bad';
+        rePassword = 'wrongInput';
     }else{rePassword = event.target.value;}
 });
 
